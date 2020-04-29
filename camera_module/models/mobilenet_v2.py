1be2112adbdb447429b86a58ee4d2695634e0c3f
haoluong <haophamluong@gmail.com>
2020-04-30 00:12:47 +0700

    delete unuse files

--- camera_module/models/mobilenet_v2.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-print(tf.__version__)
-def create_mbv2_model(checkpoint_path):
-  model = tf.keras.models.load_model(checkpoint_path)
-  return tf.keras.Sequential(model.layers[:2])
